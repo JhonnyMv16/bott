c6cbe154924f0b0a0a76a94e27ba3c150643ca84
Markos vips <78027781+HACKERESOBERANO@users.noreply.github.com>
2022-03-22 22:45:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Outros
-global.owner = ['559491423691','559491423691','559491423691','559491423691','559491423691']
+global.owner = ['559491423691']
 global.sessionName = 'batSession'
 
 // Aqui você configura o prefix
@@ -22,9 +22,18 @@ global.prefa = ['/']
 global.packname = 'Karma buscas'
 global.author = '\n\n\n\n\n\n\n\n\n'
 
+global.precopv = `🟢 07 DIAS = R$ 10,00
+🟢 30 DIAS = R$ 20,00`
+global.precogrupo = `🟢 07 DIAS = R$ 20,00
+🟢 15 DIAS = R$ 30,00
+🟢 30 DIAS = R$ 45,00`
+
+global.tabela = `tabela`
 // Api que eu te mandei (coloque com o https:// mas sem o / no final)
-global.apidados = 'https://myapisecretkarma.herokuapp.com'; // Compre sua api com o markos: wa.me/559491423691
-global.apiToken = 'coloque seu token aqui'; // Compre seu token com o markos: wa.me/559491423691
+// Forma correta: https://suApi.herokuapp.com
+// Forma incorreta: suApi.herokuapp.com/
+global.apidados = 'https://apitestekarma.herokuapp.com'; // Compre sua api com o markos: wa.me/559491423691
+global.apiToken = 'seu-token'; // Compre seu token com o markos: wa.me/559491423691
 
 
 // Website Api
@@ -37,16 +46,8 @@ global.APIKeys = {
 	'https://zenzapi.xyz': 'Your Key',
 }
 
-// TABELA DE PREÇOS PARA PV
-global.precopv = `🟢 07 DIAS = R$ 10,00
-🟢 30 DIAS = R$ 20,00`
-
-// TABELA DE PREÇOS PARA GRUPO
-global.precogrupo = `🟢 07 DIAS = R$ 20,00
-🟢 15 DIAS = R$ 30,00
-🟢 30 DIAS = R$ 45,00`
 // Other
-global.premium = ['559491423691'] // não é o sistema de premium
+global.premium = ['556599081355'] // não é o sistema de premium
 
 
 global.sp = '⭔'
@@ -56,15 +57,15 @@ global.mess = {
     botAdmin: 'Bot deve ser administrador primeiro!',
     owner: 'Recursos especiais do proprietário do bot',
     group: 'Recurso usado apenas para grupos!',
-    private: 'Recursos usados apenas para bate-papo privado!',
+    private: 'Recursos usados ​​apenas para bate-papo privado!',
     bot: 'Recursos especiais do usuário do número do bot',
     wait: 'Aguarde...',
-    endLimit: 'Seu limite diário expirou, o limite será redefinido a cada 12 horas',
+    endLimit: 'Sistema anti-flood, espere 2 segundos',
 }
 
 global.limitawal = {
     premium: "Infinity",
-    free: 100
+    free: 1
 }
 global.thumb = fs.readFileSync('./lib/bat.jpg')
 
